52eb9a736cbd233e73dc426367d03b8b9d719575
pratyushbh12 <pratyush.bhatnagar@joshtechnologygroup.com>
2025-07-17 08:12:16 +0530

    PB_RU_00:Changed husky files to fix husky issue

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
-/* This configuration applies Prettier formatting and ESLint linting to all staged `.ts` and `.tsx` files */
+/* This configuration applies Prettier formatting and ESLint linting to all staged `.js`, `.html` and `.css. files */
 module.exports={
     'src/**/*.{js,html,css}': `eslint .`,
     '*': `prettier --write ./`,
