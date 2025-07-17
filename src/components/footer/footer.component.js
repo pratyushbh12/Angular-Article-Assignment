df3bc978e5b50081b161e75166326f71110f379a
pratyushbh12 <pratyush.bhatnagar@joshtechnologygroup.com>
2025-07-17 18:33:49 +0530

    PB_RU_00:Added missing jsdocs

--- src/components/footer/footer.component.js
+++ src/components/footer/footer.component.js
@@ -1,5 +1,10 @@
 import { footer } from '@template';
 
+/**
+ * Factory function for the footer component.
+ *
+ * @returns {object} The component definition object containing the template.
+ */
 export const footerComponent = () => {
     return {
         template: footer,
--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,5 +1,10 @@
 import { header } from '@template';
 
+/**
+ * Factory function for the header component.
+ *
+ * @returns {object} The component definition object containing the template.
+ */
 export const headerComponent = () => {
     return {
         template: header,
