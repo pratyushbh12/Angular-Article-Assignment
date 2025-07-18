7cd6275993d8595b0050136766d4e65c6ab4b5a3
pratyushbh12 <pratyush.bhatnagar@joshtechnologygroup.com>
2025-07-18 12:44:29 +0530

    PB_RU_00:Made minor fixes

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 /* This configuration applies Prettier formatting and ESLint linting to all staged `.js`, `.html` and `.css. files */
 module.exports={
-    'src/**/*.{js,html,css}': 'eslint .',
+    'src/**/*.{js,html}': 'eslint .',
     '*': 'prettier --write ./',
 };
