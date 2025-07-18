4dfc40f84bb1c5f3d4c65e68ea8f9bdd52eeddc8
pratyushbh12 <pratyush.bhatnagar@joshtechnologygroup.com>
2025-07-18 19:45:56 +0530

    PB_RU_01:Added the logic of back button and removed unnecessary event listeners. Added jsdocs and fixed the profile route

--- src/controllers/components/header/header.controller.js
+++ src/controllers/components/header/header.controller.js
@@ -1,3 +1,13 @@
+/**
+ * Controller for the main application header.
+ * Manages authentication state, language selection, and a custom navigation history for the back button.
+ *
+ * @param {object} $scope The controller's scope for data binding.
+ * @param {object} $translate Service for handling language translations and changes.
+ * @param {object} $location Service for interacting with the browser's URL.
+ * @param {object} authService Service for managing user authentication.
+ * @param {object} $rootScope The root scope, used to listen for global location change events.
+ */
 export const headerCtrl = [
     '$scope',
     '$translate',
@@ -8,18 +18,26 @@ export const headerCtrl = [
         $scope.loginBtnHandler = authService.login;
         $scope.logoutBtnHandler = authService.logout;
         $scope.isLoggedIn = authService.isLoggedIn;
-        $scope.isNavigatingBack = false;
         $scope.changeLang = $translate.use;
+        $scope.isNavigatingBack = false;
+        $scope.prevLocations = [];
         $scope.lang = $translate.proposedLanguage();
         $scope.languages = $translate.getAvailableLanguageKeys();
 
-        $rootScope.$on('$translateChangeSuccess', () => {
-            $translate('header');
-            $scope.lang = $translate.proposedLanguage();
+        $rootScope.$on('$locationChangeSuccess', (_event, newUrl, oldUrl) => {
+            if ($scope.isNavigatingBack) {
+                $scope.isNavigatingBack = false;
+                return;
+            }
+            if (newUrl != oldUrl) {
+                $scope.prevLocations.push(oldUrl.split('#')[1]);
+            }
         });
 
         $scope.backClickHandler = function () {
-            $scope.log = true;
+            const oldUrl = $scope.prevLocations.pop();
+            $scope.isNavigatingBack = true;
+            $location.path(oldUrl);
         };
     },
 ];
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,3 +1,2 @@
-export { footerCtrl } from './components/footer/footer.controller';
 export { headerCtrl } from './components/header/header.controller';
 export { TestController } from './testController/testController';
