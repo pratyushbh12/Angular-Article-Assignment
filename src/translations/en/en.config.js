164a5a8b55df78c62242ed50362d49d859a19534
pratyushbh12 <pratyush.bhatnagar@joshtechnologygroup.com>
2025-07-22 01:02:03 +0530

    PB_RU_02:Made rebasing fixes

--- src/translations/en/en.config.js
+++ src/translations/en/en.config.js
@@ -11,16 +11,18 @@ export const ENGLISH_TRANSLATION_KEYS = {
     footer: {
         footerText: '2025 Articles. All rights reserved.',
     },
-    'error':{
-        'title':'Oops!',
-        'subTitle':'Something Went Wrong',
-        'body':'We\'re sorry for the inconvenience. An unexpected technical error has occurred. Our team has been automatically notified and we are working to fix the issue.',
-        'buttonText':'Go Back To Home',
+    error: {
+        title: 'Oops!',
+        subTitle: 'Something Went Wrong',
+        // eslint-disable-next-line prettier/prettier
+        body: 'We\'re sorry for the inconvenience. An unexpected technical error has occurred. Our team has been automatically notified and we are working to fix the issue.',
+        buttonText: 'Go Back To Home',
     },
-    'notFound': {
-        'title': 'Page Not Found',
-        'subTitle': 'Error 404',
-        'body': 'Sorry, the page you are looking for doesn\'t seem to exist. It might have been moved, deleted, or the link you followed was incorrect.',
-        'buttonText':'Go Back To Home',
+    notFound: {
+        title: 'Page Not Found',
+        subTitle: 'Error 404',
+        // eslint-disable-next-line prettier/prettier
+        body: 'Sorry, the page you are looking for doesn\'t seem to exist. It might have been moved, deleted, or the link you followed was incorrect.',
+        buttonText: 'Go Back To Home',
     },
 };
