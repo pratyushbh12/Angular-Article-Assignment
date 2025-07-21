0611e46f6b62b29ed759f1d1c8ec875d23e1804d
pratyushbh12 <pratyush.bhatnagar@joshtechnologygroup.com>
2025-07-21 20:55:28 +0530

    PB_RU_00:Made the fixes for prettier rules

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,6 @@
  * ESLint linting to all staged `.js`, `.html` and `.css. files
  */
 module.exports = {
-    'src/**/*.{js,html}': 'eslint --fix',
-    '**/*.{js,html,css,json,md}': 'prettier --write',
+    '*': 'eslint --fix',
+    '**/*': 'prettier --write',
 };
